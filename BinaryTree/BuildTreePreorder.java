41589fbd76019a1d183aefe4bf0f9786513bae39
Suman Panigrahi <psuman1406@gmail.com>
2023-12-06 10:01:58 +0530

    Binary tree part 2 completed

--- BinaryTree/BuildTreePreorder.java
+++ BinaryTree/BuildTreePreorder.java
@@ -120,6 +120,116 @@ public class BuildTreePreorder {
 
             return leftSum + rightSum + root.data;
         }
+
+        // sum of nodes at kth level
+        public int sumOfNodesAtKthLevel(Node root, int k) {
+            if(root == null) {
+                return 0;
+            }
+
+            if(k == 0) {
+                return root.data;
+            }
+
+            int leftSum = sumOfNodesAtKthLevel(root.left, k - 1);
+            int rightSum = sumOfNodesAtKthLevel(root.right, k - 1);
+
+            return leftSum + rightSum;
+        }
+
+        // diameter of a tree -- approach 1
+        public int diameter(Node root) {
+            if(root == null) {
+                return 0;
+            }
+
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+
+            int leftDiameter = diameter(root.left);
+            int rightDiameter = diameter(root.right);
+
+            return Math.max(leftHeight + rightHeight + 1, Math.max(leftDiameter, rightDiameter));
+        }
+
+        // diameter of a tree -- approach 2 -- return diameter and height in the same function
+        public int[] diameter2(Node root) {
+            if(root == null) {
+                int[] arr = new int[2];
+                arr[0] = 0;
+                arr[1] = 0;
+                return arr;
+            }
+
+            int[] leftAns = diameter2(root.left);
+            int[] rightAns = diameter2(root.right);
+
+            int[] ans = new int[2];
+            ans[0] = Math.max(leftAns[0], Math.max(rightAns[0], leftAns[1] + rightAns[1] + 1));
+            ans[1] = Math.max(leftAns[1], rightAns[1]) + 1;
+
+            return ans;
+        }
+
+        // isIdentical
+        public boolean isIdentical(Node root1, Node root2) {
+            if(root1 == null && root2 == null) {
+                return true;
+            }
+
+            if(root1 == null || root2 == null) {
+                return false;
+            }
+
+            if(root1.data != root2.data) {
+                return false;
+            }
+
+            boolean leftAns = isIdentical(root1.left, root2.left);
+            boolean rightAns = isIdentical(root1.right, root2.right);
+
+            return leftAns && rightAns;
+        }
+
+        // subtree of another tree
+        public boolean isSubtree(Node root, Node subRoot) {
+            if(root == null) {
+                return false;
+            }
+
+            if(subRoot == null) {
+                return true;
+            }
+
+            if(isIdentical(root, subRoot)) {
+                return true;
+            }
+
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        }
+
+        // Top View of a tree
+        public void topView(Node root) {
+            if(root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+
+            while(!q.isEmpty()) {
+                Node curr = q.poll();
+                System.out.print(curr.data + " ");
+
+                if(curr.left != null) {
+                    q.add(curr.left);
+                }
+
+                if(curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -146,5 +256,17 @@ public class BuildTreePreorder {
         System.out.println(tree.countNodes(root));
 
         System.out.println(tree.sumOfNodes(root));
+
+        System.out.println(tree.sumOfNodesAtKthLevel(root, 2));
+
+        System.out.println(tree.diameter(root));
+
+        System.out.println(tree.diameter2(root)[0]);
+
+        System.out.println(tree.isIdentical(root, root));
+
+        System.out.println(tree.isSubtree(root, root));
+
+        tree.topView(root);
     }
 }
